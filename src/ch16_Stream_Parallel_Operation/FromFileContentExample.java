3aecf815a40b820e20beee9e9419854e0ec5bd7c
TAEK LIM <lt@recobell.com>
2019-03-27 14:23:39 +0900

    ADD: ch16_...

---
+++ src/ch16_Stream_Parallel_Operation/FromFileContentExample.java
@@ -0,0 +1,10 @@
+package ch16_Stream_Parallel_Operation;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FromFileContentExample {
+	public static void main(String[] args) {
+		Path path = Paths.get("");
+	}
+}
---
+++ src/ch16_Stream_Parallel_Operation/LambdaExpressionsExample.java
@@ -0,0 +1,26 @@
+package ch16_Stream_Parallel_Operation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LambdaExpressionsExample {
+	public static void main(String[] args) {
+		List<Student> list = Arrays.asList(
+				new Student("victor", 99), new Student("dio", 100)
+		);
+		Stream<Student> stream = list.stream();
+		stream.forEach(s -> {
+			String name = s.getName();
+			int score = s.getScore();
+			System.out.println(name + " = " + score);
+		});
+		
+		Arrays
+			.stream(new int[] {1, 2, 3, 4, 5})
+			.boxed()
+			.forEach(System.out::println);
+		
+		
+	}
+}
